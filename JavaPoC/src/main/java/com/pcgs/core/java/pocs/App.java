a118326f5d20c387ba7c55995f3b020991afc1a7
San Gan <32886735+PSANGAN@users.noreply.github.com>
2025-11-28 19:29:19 -0500

    Init

---
+++ JavaPoC/src/main/java/com/pcgs/core/java/pocs/App.java
@@ -0,0 +1,13 @@
+package com.pcgs.core.java.pocs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
